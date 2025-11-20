dc67d02d137044a60285296f9c8c00135e052763
Premtim <premtim.berisha@hotmail.com>
2025-11-20 20:09:18 +0100

    Updating the functions to the calculator

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-function add(a,b){
-    return a+b;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(a,b){
-    return a*b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a,b){
-    return a/b;
+function divide(a, b) {
+  return a / b;
 }
 
-console.log(add(3,5));
-console.log(subtract(2,1));
-console.log(multiply(2,2));
-console.log(divide(6,3));
 
 let firstNum = '';
 let operator = '';
@@ -39,25 +35,10 @@ function operate(op,num1,num2){
 
 }
 
-console.log(operate("+",3,5));
-console.log(operate('-',4,2));
-console.log(operate('*',4,4));
-console.log(operate('/',3,2));
-
-let buttons = document.querySelectorAll('.buttons button');
 let display = document.querySelector('#display');
-
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const value = button.textContent;
-        if(display.textContent === '0'){
-            display.textContent = value;
-        } else {
-            display.textContent += value;
-        }
-    });
-});
+let buttons = document.querySelectorAll('.buttons button');
+let clear = document.querySelector('.clear');
+let backspace = document.querySelector('.backspace');
 
 function updateDisplay(value){
     if(value === '' || value === null){
@@ -66,21 +47,83 @@ function updateDisplay(value){
         display.textContent = value;
     }
 }
+updateDisplay('0');
 
-let clear = document.querySelector('.clear');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.textContent.trim();
+        if(/\d/.test(value) || value === '.'){
+            handleDigit(value);
+        } else if ('*/+-'.includes(value)){
+            handleOperator(value);
+        } else if (value === '=') {
+            handleEquals();
+        }
+    });
+});
+
+function handleDigit(digit){
+    if(operator === ''){
+        if(firstNum === '0' && digit !== '.'){
+            firstNum = digit;
+        } else {
+            if(digit === '.' && firstNum.includes('.')) return;
+            firstNum += digit;
+        }
+        updateDisplay(firstNum);
+    } else {
+        if(secondNum === '0' && digit !== '.'){
+            secondNum = digit;
+        } else {
+            if(digit === '.' && secondNum.includes('.')) return;
+            secondNum += digit;
+        } 
+        updateDisplay(secondNum);
+    }
+}
+
+function handleOperator(op){
+    if(firstNum === '') return;
+
+    if(secondNum !== ''){
+        const result = operate(operator,Number(firstNum),Number(secondNum))
+        firstNum = String(result);
+        secondNum = '';
+        updateDisplay(firstNum);
+    }
+    operator = op;
+}
+
+function handleEquals(){
+    if(firstNum === '' || operator === '' || secondNum === '') return;
+    const result = operate(operator, Number(firstNum), Number(secondNum));
+    updateDisplay(result);
+
+    firstNum = String(result);
+    operator = '';
+    secondNum = '';
+}
+
 clear.addEventListener('click', () => {
     firstNum = '';
     operator = '';
     secondNum = '';
-    display.textContent = '0';
+    updateDisplay('0');
 });
 
-let backspace = document.querySelector('.backspace');
 backspace.addEventListener('click', () => {
-    let current = display.textContent;
-    if(current.length > 1){
-        updateDisplay(current.slice(0, -1));
-    }else {
-        updateDisplay('0');
+    if(operator === ''){
+        if(firstNum.length > 0){
+            firstNum = firstNum.slice(0,-1);
+            updateDisplay(firstNum);
+        } else {
+            updateDisplay('');
+        }
+    } else if (secondNum !== ''){
+            secondNum = secondNum.slice(0,-1);
+            updateDisplay(secondNum);
+    } else {
+        operator = '';
+        updateDisplay(firstNum);
     }
 });
